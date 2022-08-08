c392e96024658ceeb444b47b7c3701c77039c28f
est8r <estermessias6@gmail.com>
2022-08-08 00:19:01 -0300

    iniciando o resumo sobre herança e protótipos

---
+++ content-reviewing/objects/objects2.js
@@ -0,0 +1,15 @@
+//PROTÓTIPO
+
+//HERANÇA
+/*todos os objetos que criamos herdam propriedades e métodos
+de um objeto nativo já criado e armazenado pelo javascript,
+então, alem de termos nossos atos criados, temos os que
+herdamos do Object() já existente, ex:*/
+
+let sapo = {
+  cor: 'verde',
+  habitat: 'lagoa',
+  região: 'mata atlantica',
+}; //sapo.hasOwnProperty('cor') no console, retorna true
+
+//strings comuns também possuem propriedades e métodos acessiveis
